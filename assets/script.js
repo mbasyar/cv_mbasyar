b4745fbc323b5e83dee3987f64939f2945e62348
ferdylimmm9 <ferdylimm9@gmail.com>
2022-05-25 23:13:55 +0700

    initialize portfolio

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function overlay() {
+  //dapatkan element by id menu
+  var overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+}
+
+function overlayClick() {
+  var overlay = document.getElementById("overlay");
+
+  overlay.style.display = "none";
+}
+
+window.onresize = overlayClick;
